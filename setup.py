32a89dc50eb976ec2f897403e799800cde99d726
Numa Dhamani <numadhamani@gmail.com>
2019-02-04 13:04:34 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from distutils.core import setup
 
 setup(name='RffeaturesD3MWrapper',
-    version='3.0.0',
+    version='3.1.0',
     description='A wrapper for running the punk rffeatures functions in the d3m environment.',
     packages=['RffeaturesD3MWrapper'],
     install_requires=["numpy",
         "pandas == 0.23.4",
-        "requests",
+        "requests >= 2.18.4, <= 2.20.0",
         "typing",
         "punk==3.0.0"],
     dependency_links=[
     ],
     entry_points = {
         'd3m.primitives': [
-            'distil.rffeatures = RffeaturesD3MWrapper:rffeatures'
+            'feature_selection.rffeatures.Rffeatures = RffeaturesD3MWrapper:rffeatures'
         ],
     },
 )
