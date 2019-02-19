47140fa691d4c8fb8be858e60eb5e90561052564
Numa Dhamani <numadhamani@gmail.com>
2019-02-19 11:18:09 -0600

    bumping version to 3.1.1

--- RffeaturesD3MWrapper/wrapper.py
+++ RffeaturesD3MWrapper/wrapper.py
@@ -18,8 +18,8 @@ from d3m.metadata import hyperparams, base as metadata_base, params
 from common_primitives import dataset_to_dataframe as DatasetToDataFrame
 
 __author__ = 'Distil'
-__version__ = '3.1.0'
-__contact__ = 'mailto:numa@newknowledge.io'
+__version__ = '3.1.1'
+__contact__ = 'mailto:nklabs@newknowledge.io'
 
 Inputs = container.pandas.DataFrame
 Outputs = container.pandas.DataFrame
