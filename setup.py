71dbe71b4fa661365a28a7abd318712de45c3d78
Numa Dhamani <numadhamani@gmail.com>
2019-11-19 12:23:23 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='RffeaturesD3MWrapper',
-    version='3.1.1',
+    version='3.1.2',
     description='A wrapper for running the punk rffeatures functions in the d3m environment.',
     packages=['RffeaturesD3MWrapper'],
     install_requires=["numpy",
